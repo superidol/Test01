e98a933ff15003482b212aea1ff9c065803b8f63
Master-superidol <cs.power.supply@outlook.com>
2019-02-15 11:58:55 +0800

    Mess01

--- test01.py
+++ test01.py
@@ -174,16 +174,3 @@ result = func(func(tw))
 
 
 ''' 
-def list_all_files(rootdir):
-    _files = []
-    list = os.listdir(rootdir) #列出文件夹下所有的目录与文件
-    for i in range(0,len(list)):
-           path = os.path.join(rootdir,list[i])
-           if os.path.isdir(path):
-              _files.extend(list_all_files(path))
-           if os.path.isfile(path):
-              _files.append(path)
-    return _files
-_fs = list_all_files('./Test01')
-#将第一阶段的文件遍历出来
-_k = filter(lambda x:re.compile(r'stage2.txt').search(x),_fs)
