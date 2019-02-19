49b2d5431b8b5688c57dbe13cc7503a6d504926d
Master-superidol <cs.power.supply@outlook.com>
2019-02-19 10:41:28 +0800

    Mess01

--- test.sql
+++ test.sql
@@ -3,3 +3,6 @@ select * from ydd where 2=3;
 select * from ydd where 3=4;
 select * from ydd where 4=5;
 select * from ydd where 5=7;
+select * from ydd where 6=7;
+select * from ydd where 8=9;
+select * from ydd where 9=0;
