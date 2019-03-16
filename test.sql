69a5fb5f9cc0b5420d8e6cd16a36169e74960a3a
Superidol-MacOS <superidol@me.com>
2019-03-16 11:36:18 +0800

    2019-3-16 test

--- test.sql
+++ test.sql
@@ -10,5 +10,6 @@ select * from ydd where 9=0;
 =======
 select * from ydd where 7=8;
 select * from ydd where 8=9;
+select * from ydd where 00=0090
 end
 >>>>>>> 1ef1f1bb21b5acf9175d310d595dbb7a1e3525cf
